55a6ef123647637a54cee359bb6dcfa993880a1e
RexGene <rexgene@hotmail.com>
2017-10-24 16:24:48 +0800

    实现websocket连接层

--- net_protocol/websocket.go
+++ net_protocol/websocket.go
@@ -1,26 +1,63 @@
 package net_protocol
 
-import ()
+import (
+	"golang.org/x/net/websocket"
+	"net/http"
+	"time"
+)
+
+const (
+	READ_TIMEOUT_S   = 10
+	WRITE_TIMEOUT_S  = 10
+	MAX_HEADER_BYTES = 1 << 20
+)
 
 type WebSocket struct {
+	conn *websocket.Conn
 }
 
-func NewWebSocket() *WebSocket {
-	return &WebSocket{}
+func NewWebSocket(conn *websocket.Conn) *WebSocket {
+	return &WebSocket{
+		conn: conn,
+	}
 }
 
 func (self *WebSocket) Read(data []byte) (int, error) {
-	return 0, nil
+	return self.conn.Read(data)
 }
 
 func (self *WebSocket) Write(data []byte) (int, error) {
-	return 0, nil
+	return self.conn.Write(data)
 }
 
 func (self *WebSocket) Close() error {
-	return nil
+	return self.conn.Close()
 }
 
-func Listen(addr string) (*WebSocket, error) {
-	return nil, nil
+type WebSocketListener struct {
+	server *http.Server
+}
+
+func NewWebSocketListener() *WebSocketListener {
+	return &WebSocketListener{}
+}
+
+func (self *WebSocketListener) Listen(addr string, handler func(INetProtocol)) error {
+	onHandle := func(conn *websocket.Conn) {
+		handler(NewWebSocket(conn))
+	}
+
+	s := &http.Server{
+		Addr:           addr,
+		Handler:        websocket.Handler(onHandle),
+		ReadTimeout:    READ_TIMEOUT_S * time.Second,
+		WriteTimeout:   WRITE_TIMEOUT_S * time.Second,
+		MaxHeaderBytes: MAX_HEADER_BYTES,
+	}
+	self.server = s
+	return s.ListenAndServe()
+}
+
+func (self *WebSocketListener) Close() error {
+	return self.server.Close()
 }
