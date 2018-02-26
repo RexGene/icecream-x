3d95d3843bf35e745fd1d955d6b62e1d8d503ed4
RexGene <rexgene@hotmail.com>
2018-02-26 23:29:34 +0800

    替换websocket库

--- net_protocol/websocket.go
+++ net_protocol/websocket.go
@@ -1,8 +1,11 @@
 package net_protocol
 
 import (
-	websocket "github.com/RexGene/websocket-go"
+	// websocket "github.com/RexGene/websocket-go"
 	// "golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
 	"net/http"
 	"time"
 )
@@ -13,8 +16,32 @@ const (
 	MAX_HEADER_BYTES = 1 << 20
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:    4096,
+	WriteBufferSize:   4096,
+	EnableCompression: true,
+	// CheckOrigin: func(r *http.Request) bool {
+	// 	return true
+	// },
+}
+
+type Handler func(conn *websocket.Conn)
+
+func (self Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("[-] Upgrade:", err)
+		return
+
+	}
+
+	defer conn.Close()
+	self(conn)
+}
+
 type WebSocket struct {
-	conn *websocket.Conn
+	conn       *websocket.Conn
+	lastReader io.Reader
 }
 
 func NewWebSocket(conn *websocket.Conn) *WebSocket {
@@ -24,23 +51,43 @@ func NewWebSocket(conn *websocket.Conn) *WebSocket {
 }
 
 func (self *WebSocket) Read(data []byte) (int, error) {
-	return self.conn.Read(data)
+	if self.lastReader != nil {
+		ret, err := self.lastReader.Read(data)
+		if err == nil {
+			return ret, err
+		}
+
+		if err != io.EOF {
+			log.Println("[!]", err)
+		}
+		self.lastReader = nil
+	}
+
+	_, r, err := self.conn.NextReader()
+	if err != nil {
+		return 0, err
+	}
+
+	self.lastReader = r
+	return r.Read(data)
 }
 
 func (self *WebSocket) Write(data []byte) (int, error) {
-	return self.conn.Write(data)
+	w, err := self.conn.NextWriter(websocket.BinaryMessage)
+	if err != nil {
+		return 0, err
+	}
+
+	defer w.Close()
+	return w.Write(data)
 }
 
 func (self *WebSocket) Close() error {
-	body := self.conn.Request().Body
-	if body != nil {
-		body.Close()
-	}
 	return self.conn.Close()
 }
 
 func (self *WebSocket) GetRemoteAddr() string {
-	return self.conn.Request().RemoteAddr
+	return self.conn.RemoteAddr().String()
 }
 
 func (self *WebSocket) GetLocalAddr() string {
@@ -66,7 +113,7 @@ func (self *WebSocketListener) Listen(addr string, handler func(INetProtocol)) e
 
 	s := &http.Server{
 		Addr:           addr,
-		Handler:        websocket.Handler(onHandle),
+		Handler:        Handler(onHandle),
 		ReadTimeout:    READ_TIMEOUT_S * time.Second,
 		WriteTimeout:   WRITE_TIMEOUT_S * time.Second,
 		MaxHeaderBytes: MAX_HEADER_BYTES,
@@ -99,7 +146,7 @@ func (self *WebSocketSecureListener) Listen(addr string, handler func(INetProtoc
 
 	s := &http.Server{
 		Addr:           addr,
-		Handler:        websocket.Handler(onHandle),
+		Handler:        Handler(onHandle),
 		ReadTimeout:    READ_TIMEOUT_S * time.Second,
 		WriteTimeout:   WRITE_TIMEOUT_S * time.Second,
 		MaxHeaderBytes: MAX_HEADER_BYTES,
